b958336ae0fafc713d47893ec116525c5664f173
Olivier Laurent <olivier.laurent@ensta-paris.fr>
2020-12-12 12:36:59 +0100

    enabled different types of light corrected reflexion

--- src/Object.hpp
+++ src/Object.hpp
@@ -6,7 +6,9 @@
 #include <glm/geometric.hpp>
 #include <glm/gtx/intersect.hpp>
 #include <glm/vec3.hpp>
+#include <algorithm>
 #include "Ray.hpp"
+
 
 //!  The core Object class.
 /*!
@@ -14,163 +16,193 @@
 */
 struct PhysicalObject
 {
-  //! A public variable.
-  /*!
+    //! A public variable.
+    /*!
       pos is the 3D location of the object.
     */
-  glm::vec3 pos;
+    glm::vec3 pos;
 
-  //! A pure virtual function returning the information about an object.
-  /*!
+    //! A pure virtual function returning the information about an object.
+    /*!
       \param os the current ostream.
       \return the information of the object as an ostream
     */
-  virtual std::ostream &printInfo(std::ostream &os) const = 0;
+    virtual std::ostream &printInfo(std::ostream &os) const = 0;
 
-  //! The default constructor.
-  /*!
+    //! The default constructor.
+    /*!
       It puts the object in (0,0,0).
     */
-  explicit PhysicalObject(glm::vec3 pos = glm::vec3()) : pos(pos) {}
+    explicit PhysicalObject(glm::vec3 pos = glm::vec3()) : pos(pos) {}
 
-  friend std::ostream &operator<<(std::ostream &stream, PhysicalObject const &obj)
-  {
-    return obj.printInfo(stream);
-  }
+    friend std::ostream &operator<<(std::ostream &stream, PhysicalObject const &obj)
+    {
+        return obj.printInfo(stream);
+    }
 };
 
 //!  The light-source Object class.
 /*!
   The object represents a light-source, which is located at pos.
 */
-struct LightSource : public PhysicalObject
+struct Light : public PhysicalObject
 {
 
-  //! A public variable.
-  /*!
+    //! A public variable.
+    /*!
       color represents the color of the light-source. color elements are between 0 and 1.
     */
-  glm::vec3 color;
-  float intensity;
-  //! A normal member taking one argument and returning the information about an object.
-  /*!
+    cv::Vec3b color;
+    float intensity;
+    //! A normal member taking one argument and returning the information about an object.
+    /*!
       \param os the current ostream
       \return The information of the object as an ostream
     */
-  std::ostream &printInfo(std::ostream &os) const;
+    std::ostream &printInfo(std::ostream &os) const;
 
-  //! The default constructor.
-  /*!
+    virtual float outboundRay(const glm::vec3 hitPt, Ray &ray, cv::Vec3b &rayColor) const = 0;
+    //! The default constructor.
+    /*!
       It puts the object in (0, 0, 0), and selects (0, 0, 0) as the color of the source.
     */
-  explicit LightSource(glm::vec3 pos = glm::vec3(), glm::vec3 color = glm::vec3(1, 1, 1)) : PhysicalObject(pos), color(color), intensity(3) {}
+    explicit Light(glm::vec3 pos = glm::vec3(), cv::Vec3b color = cv::Vec3b(1, 1, 1), float i=1) : PhysicalObject(pos),
+                                                                                                 color(color), intensity(i)
+    {
+    }
+};
+
+struct directLight : public Light
+{
+    float outboundRay(const glm::vec3 hitPt, Ray &ray, cv::Vec3b &rayColor) const
+    {
+        ray.initPt = hitPt;
+        ray.dir = glm::normalize(pos - hitPt);
+        rayColor = color * intensity;
+        return glm::distance(hitPt, pos);
+    }
+    explicit directLight(glm::vec3 pos = glm::vec3(), cv::Vec3b color = cv::Vec3b(1, 1, 1), float i = 3) : Light(pos, color, i) {}
+};
+
+struct spotLight : public Light
+{
+    float outboundRay(const glm::vec3 hitPt, Ray &ray, cv::Vec3b &rayColor) const
+    {
+        ray.initPt = hitPt;
+        ray.dir = glm::normalize(pos - hitPt);
+        rayColor = detail::glm2cv(detail::cv2glm(color) * std::min(255.0f, (float)(intensity / (4 * M_PI * glm::dot(pos - hitPt, pos - hitPt)))));
+        //std::cout << intensity <<" "<<rayColor << std::endl;
+        return glm::distance(hitPt, pos);
+    }
+
+    explicit spotLight(glm::vec3 pos = glm::vec3(), cv::Vec3b color = cv::Vec3b(1, 1, 1), float i = 10000) : Light(pos, color, i) {}
 };
 
 struct ObjectBase : public PhysicalObject
 {
-  //! A public variable.
-  /*!
+    //! A public variable.
+    /*!
       color represents the color of the light-source. color elements are between 0 and 1.
     */
-  glm::vec3 color;
+    cv::Vec3b color;
 
-  float transparency;
-  float refractiveIndex;
-  float reflexionIndex;
-  float albedo;
+    float transparency;
+    float refractiveIndex;
+    float reflexionIndex;
+    float albedo;
 
-  virtual std::vector<Ray> intersect(const Ray ray, const LightSource ltSrc, float &distance, glm::vec3 &normal) = 0;
+    virtual std::vector<Ray> intersect(const Ray ray, const std::shared_ptr<Light> ltSrc, float &iDistance, float &lDistance, glm::vec3 &normal, cv::Vec3b &rColor) = 0;
 
-  explicit ObjectBase(glm::vec3 pos = glm::vec3(), glm::vec3 color = glm::vec3(1, 1, 1), float t = 0, float r = 1, float R = 1, float a = 0.18) : PhysicalObject(pos), color(color), transparency(t), refractiveIndex(r), reflexionIndex(R), albedo(a)
-  {
-    std::cout << pos << std::endl;
-  }
-  //explicit ObjectBase(glm::vec3 pos, float t, float r, float a) : PhysicalObject(pos), transparency(t), refractiveIndex(r), albedo(a) {}
+    explicit ObjectBase(glm::vec3 pos = glm::vec3(), cv::Vec3b color = cv::Vec3b(1, 1, 1), float t = 0, float r = 1, float R = 1, float a = 0.18) : PhysicalObject(pos), color(color), transparency(t), refractiveIndex(r), reflexionIndex(R), albedo(a)
+    {
+        std::cout << pos << std::endl;
+    }
+    //explicit ObjectBase(glm::vec3 pos, float t, float r, float a) : PhysicalObject(pos), transparency(t), refractiveIndex(r), albedo(a) {}
 };
 
 struct Camera : public PhysicalObject
 {
-  //! A public variable.
-  /*!
+    //! A public variable.
+    /*!
       Direction of the camera.
     */
-  glm::vec3 dir;
+    glm::vec3 dir;
 
-  // Size of the screen
-  float sizeX;
-  float sizeY;
+    // Size of the screen
+    float sizeX;
+    float sizeY;
 
-  // Resolution of the screen
-  unsigned resX;
-  unsigned resY;
+    // Resolution of the screen
+    unsigned resX;
+    unsigned resY;
 
-  float focalLength;
+    float focalLength;
 
-  //! A normal member taking two arguments and returning the generated ray
-  /*!
+    //! A normal member taking two arguments and returning the generated ray
+    /*!
       \param x the number of the x pixel
       \return the number of the y pixel
     */
-  Ray genRay(unsigned x, unsigned y);
+    Ray genRay(unsigned x, unsigned y);
 
-  //! A normal member taking one argument and returning the information about an object.
-  /*!
+    //! A normal member taking one argument and returning the information about an object.
+    /*!
       \param os the current osstream
       \return The information of the object as an ostream
     */
-  std::ostream &printInfo(std::ostream &os) const;
+    std::ostream &printInfo(std::ostream &os) const;
 
-  //! The default constructor.
-  /*!
+    //! The default constructor.
+    /*!
       Creates a Camera at (0, 0, 0) with screen of size (1, 1) and (1000, 1000) pixels 1 away from the image sensor.
     */
-  explicit Camera(glm::vec3 pos = glm::vec3(), float sx = 1, float sy = 1, unsigned rx = 1000, unsigned ry = 1000, float fL = 1) : PhysicalObject(pos), sizeX(sx), sizeY(sy),
-                                                                                                                                   resX(rx), resY(ry), focalLength(fL) {}
+    explicit Camera(glm::vec3 pos = glm::vec3(), float sx = 1, float sy = 1, unsigned rx = 1000, unsigned ry = 1000, float fL = 1) : PhysicalObject(pos), sizeX(sx), sizeY(sy),
+                                                                                                                                     resX(rx), resY(ry), focalLength(fL) {}
 };
 
 struct Plane : public ObjectBase
 {
-  //! A public variable.
-  /*!
+    //! A public variable.
+    /*!
       The normal vector of the plane
     */
-  glm::vec3 normal; //
+    glm::vec3 normal; //
 
-  std::vector<Ray> intersect(const Ray ray, const LightSource ltSrc, float &distance, glm::vec3 &normal);
+    std::vector<Ray> intersect(const Ray ray, const std::shared_ptr<Light> ltSrc, float &iDistance, float &lDistance, glm::vec3 &normal, cv::Vec3b &rColor);
 
-  //! A normal member taking one argument and returning the information about an object.
-  /*!
+    //! A normal member taking one argument and returning the information about an object.
+    /*!
       \param os the current osstream
       \return The information of the object as an ostream
     */
-  std::ostream &printInfo(std::ostream &os) const;
+    std::ostream &printInfo(std::ostream &os) const;
 
-  //! The default constructor.
-  /*Creates a Plan of normal (0, 0, 1) and containing (0, 0, 0) by default.
+    //! The default constructor.
+    /*Creates a Plan of normal (0, 0, 1) and containing (0, 0, 0) by default.
     */
-  explicit Plane(glm::vec3 pos = glm::vec3(), glm::vec3 color = glm::vec3(1, 1, 1), glm::vec3 normal = glm::vec3(0, 0, 1), float t = 0, float r = 1, float R = 1, float a = 0.18) : ObjectBase(pos, color, t, r, R, a), normal(glm::normalize(normal)) {}
+    explicit Plane(glm::vec3 pos = glm::vec3(), cv::Vec3b color = cv::Vec3b(1, 1, 1), glm::vec3 normal = glm::vec3(0, 0, 1), float t = 0, float r = 1, float R = 1, float a = 0.18) : ObjectBase(pos, color, t, r, R, a), normal(glm::normalize(normal)) {}
 };
 
 struct Sphere : public ObjectBase
 {
-  //! A public variable.
-  /*!
+    //! A public variable.
+    /*!
       The radius of the sphere
     */
-  float radius;
+    float radius;
 
-  std::vector<Ray> intersect(const Ray ray, const LightSource ltSrc, float &distance, glm::vec3 &normal);
+    std::vector<Ray> intersect(const Ray ray, const std::shared_ptr<Light> ltSrc, float &iDistance, float &lDistance, glm::vec3 &normal, cv::Vec3b &rColor);
 
-  //! A normal member taking one argument and returning the information about an object.
-  /*!
+    //! A normal member taking one argument and returning the information about an object.
+    /*!
       \param os the current osstream
       \return The information of the object as an ostream
     */
-  std::ostream &printInfo(std::ostream &os) const;
+    std::ostream &printInfo(std::ostream &os) const;
 
-  //! The default constructor.
-  /*!
+    //! The default constructor.
+    /*!
       Creates a Sphere in (0, 0, 0) of radius 1 by default.
     */
-  explicit Sphere(glm::vec3 pos = glm::vec3(), glm::vec3 color = glm::vec3(1, 1, 1), float radius = 1, float t = 0, float r = 1, float R = 1, float a = 0.18) : ObjectBase(pos, color, t, r, R, a), radius(radius) {}
+    explicit Sphere(glm::vec3 pos = glm::vec3(), cv::Vec3b color = cv::Vec3b(1, 1, 1), float radius = 1, float t = 0, float r = 1, float R = 1, float a = 0.18) : ObjectBase(pos, color, t, r, R, a), radius(radius) {}
 };
--- src/Scene.cpp
+++ src/Scene.cpp
@@ -1,49 +1,60 @@
 #include "Scene.hpp"
 #include <algorithm>
-#define MAX_DEPTH 1
+#define MAX_DEPTH 30
 #include <opencv2/opencv.hpp>
 
-cv::Vec3b castRay(Ray const &ray, std::shared_ptr<LightSource> const &lightSource, std::vector<std::shared_ptr<ObjectBase>> const &objects, const int &depth = 0)
+cv::Vec3b castRay(Ray const &ray, std::shared_ptr<Light> const &lightSource, std::vector<std::shared_ptr<ObjectBase>> const &objects, const int &depth = 0)
 {
     cv::Vec3b color(0, 0, 0);
     if (depth > MAX_DEPTH)
         return color;
     else
     {
+        //std::cout << std::endl << std::endl;
         std::vector<Ray> shadowRays;
         glm::vec3 hitNormal;
-        float distance = INFINITY;
+        cv::Vec3b rColor;
+        float closestDistance = INFINITY;
+        float lDistance; //light distance
+        float iDistance; //intersection distance
         std::shared_ptr<ObjectBase> hitObject = nullptr;
         for (auto object : objects)
         {
-            float cDistance;
-            shadowRays = object->intersect(ray, *lightSource, cDistance, hitNormal);
-            if (shadowRays.size() && cDistance < distance)
+            shadowRays = object->intersect(ray, lightSource, iDistance, lDistance, hitNormal, rColor);
+            //std::cout << iDistance << std::endl;
+            if (shadowRays.size() && iDistance < closestDistance)
             {
                 hitObject = object;
-                distance = cDistance;
+                closestDistance = iDistance;
             }
         }
         if (hitObject)
         {
-            shadowRays = hitObject->intersect(ray, *lightSource, distance, hitNormal);
+
+            // Calcul des rayons de diffusion
+
+            shadowRays = hitObject->intersect(ray, lightSource, iDistance, lDistance, hitNormal, rColor);
+            //std::cout << ray << std::endl
+            //          << shadowRays[0] << std::endl;
             // Delete acne
+            //std::cout << hitNormal << std::endl;
             shadowRays[0].initPt[0] = hitNormal[0] * 0.0001 + shadowRays[0].initPt[0];
             shadowRays[0].initPt[1] = hitNormal[1] * 0.0001 + shadowRays[0].initPt[1];
             shadowRays[0].initPt[2] = hitNormal[2] * 0.0001 + shadowRays[0].initPt[2];
 
             bool blocked = false;
-            float cDistance;
+            float iDistance; // distance with the intersection
 
             std::vector<Ray> sRays;
             glm::vec3 norm;
 
+            cv::Vec3b _rColor; // unsaved
             for (auto object : objects)
             {
-                sRays = object->intersect(shadowRays[0], *lightSource, cDistance, norm);
+                sRays = object->intersect(shadowRays[0], lightSource, iDistance, lDistance, norm, _rColor);
 
                 //Si le rayon est obstrué avant la source lumineuse
-                if (sRays.size() and cDistance < glm::distance(lightSource->pos, shadowRays[0].initPt))
+                if (sRays.size() and iDistance < lDistance)
                 {
                     /*std::cout << *object << std::endl;
                     std::cout << ray << std::endl
@@ -53,25 +64,25 @@ cv::Vec3b castRay(Ray const &ray, std::shared_ptr<LightSource> const &lightSourc
                     blocked = true;
                 }
             }
+            cv::Vec3b ref1;
+            cv::multiply(rColor, hitObject->color, ref1);
+            color = ref1 * (1 - hitObject->reflexionIndex) * (!blocked) * hitObject->albedo * 255 / M_PI * std::max(0.f, glm::dot(hitNormal, shadowRays[0].dir)) * 2;
 
-            color[0] = (1 - hitObject->reflexionIndex) * (!blocked) * hitObject->albedo / M_PI * lightSource->intensity * std::max(0.f, glm::dot(hitNormal, shadowRays[0].dir)) * 255 * 4 * hitObject->color[0];
-            color[1] = (1 - hitObject->reflexionIndex) * (!blocked) * hitObject->albedo / M_PI * lightSource->intensity * std::max(0.f, glm::dot(hitNormal, shadowRays[0].dir)) * 255 * 4 * hitObject->color[1];
-            color[2] = (1 - hitObject->reflexionIndex) * (!blocked) * hitObject->albedo / M_PI * lightSource->intensity * std::max(0.f, glm::dot(hitNormal, shadowRays[0].dir)) * 255 * 4 * hitObject->color[2];
-
-            // If reflective
+            // Calcul des rayons de diffusion
+            //std::cout << *hitObject/*->reflexionIndex*/ << std::endl;
             if (hitObject->reflexionIndex)
             {
                 //std::cout << "reflexion\n";
 
-                Ray reflectedRay(shadowRays[0].initPt, shadowRays[0].dir - 2 * glm::dot(shadowRays[0].dir, hitNormal) * hitNormal);
-                //std::cout << "\nInbound: " << shadowRays[0] << "\nOutbound: " << reflectedRay << std::endl;
+                Ray reflectedRay(shadowRays[0].initPt, ray.dir - 2 * glm::dot(ray.dir, hitNormal) * hitNormal);
+                //std::cout << "\nInbound: " << ray << std::endl
+                //          << hitNormal << "\nOutbound: " << reflectedRay << std::endl;
                 //if (shadowRays[0].dir[2] > -reflectedRay.dir[2] + 1e4 || shadowRays[0].dir[2] < -reflectedRay.dir[2] - 1e4)
                 //    std::cout << "\nInbound: " << shadowRays[0].dir[2] << "\nOutbound: " << reflectedRay.dir[2] << std::endl;
-                cv::Vec3b ref = castRay(reflectedRay, lightSource, objects, depth + 1);
+                cv::Vec3b ref;
+                cv::multiply(hitObject->color, castRay(reflectedRay, lightSource, objects, depth + 1), ref);
                 //std::cout << ref << std::endl;
-                color[0] += hitObject->reflexionIndex * ref[0] * hitObject->color[0];
-                color[1] += hitObject->reflexionIndex * ref[1] * hitObject->color[1];
-                color[2] += hitObject->reflexionIndex * ref[2] * hitObject->color[2];
+                color += hitObject->reflexionIndex * ref;
                 //std::cout << color << std::endl;
             }
         }
@@ -90,8 +101,11 @@ void Scene::render()
         //std::cout << x << std::endl;
         for (unsigned y = 0; y < camera->resY; ++y)
         {
+            //std::cout << y << std::endl;
+            //unsigned x = 500;unsigned y=400;
             int depth = 0;
             Ray primRay = camera->genRay(x, y);
+            //std::cout << primRay << std::endl;
             color = castRay(primRay, lightSource, objects, depth);
             image.at<cv::Vec3b>(x, y) = color;
         };
