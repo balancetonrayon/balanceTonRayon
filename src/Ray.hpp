f11315cee6aed5e8037e43e650022a3397e97896
Laurent Olivier <olivier.laurent@ensta-paris.fr>
2021-03-05 22:25:54 +0100

    Doxygen Ray

--- src/Ray.hpp
+++ src/Ray.hpp
@@ -4,61 +4,108 @@
 
 #include "utils.hpp"
 
-//!  The Ray Object class.
-/*!
-  The object ray is caracterized by an initial point and a direction
-*/
+/**
+ * @brief The object ray is caracterized by an initial point and a direction
+ * @class Ray
+ */
 struct Ray {
 protected:
-    //! A public variable.
-    /*!
-      pos is the 3D initial point.
-    */
+    /**
+     * @brief The 3D initial point.
+     *
+     */
     glm::vec3 initPt;
 
-    //! A public variable.
-    /*!
-      dir is the direction of the ray.
-    */
+    /**
+     * @brief The direction of the ray.
+     *
+     */
     glm::vec3 dir;
 
-    //! A public variable.
-    /*!
-      The color of the ray. Each element of the vec3 is contained in [0, 1].
+    /**
+     * @brief The color of the ray. Each element of the vec3 is contained in [0, 1].
+     *
      */
     glm::vec3 color;
 
 public:
+    /**
+     * @brief Get the Init Pt object
+     *
+     * @return glm::vec3
+     */
     glm::vec3 getInitPt() const { return this->initPt; }
+
+    /**
+     * @brief Get the Dir object
+     *
+     * @return glm::vec3
+     */
     glm::vec3 getDir() const { return this->dir; }
+    /**
+     * @brief Get the Color object
+     *
+     * @return glm::vec3
+     */
     glm::vec3 getColor() const { return this->color; }
 
+    /**
+     * @brief Set the Init Pt object
+     *
+     * @param initPt
+     * @return glm::vec3
+     */
     glm::vec3 setInitPt(glm::vec3 initPt) { this->initPt = initPt; }
+
+    /**
+     * @brief Set the Dir object
+     *
+     * @param dir
+     * @return glm::vec3
+     */
     glm::vec3 setDir(glm::vec3 dir) { this->dir = dir; }
+
+    /**
+     * @brief Set the Color object
+     *
+     * @param color
+     * @return glm::vec3
+     */
     glm::vec3 setColor(glm::vec3 color) { this->color = color; }
 
+    /**
+     * @brief
+     *
+     * @param hitNormal
+     * @param biaisCoeff
+     */
     void biais(glm::vec3 hitNormal, float biaisCoeff) { initPt = initPt + hitNormal * biaisCoeff; }
-    
-    // Constructors
 
-    //! The default constructor.
-    /*!
-      It creates a ray starting at 0,0,0 and going towards increasing x.
-    */
+    // Constructors
+    /** The default constructor
+    /**
+     * @brief Construct a Ray starting at 0,0,0 and going towards increasing x.
+     *
+     */
     Ray() : initPt(glm::vec3(0, 0, 0)), dir(glm::vec3(1, 0, 0)) {}
 
-    //! The specialized constructor.
-    /*!
-      It creates a Ray with specified initial point and direction.
+    /** The specialized constructor.
+    /**
+    * @brief Construct a Ray with specified initial point and direction.
+    *
+    * @param initPt
+    * @param dir
     */
     Ray(glm::vec3 initPt, glm::vec3 dir) : initPt(initPt), dir(glm::normalize(dir)) {}
 
-    //! An overload of the operator << to print rays for debug.
-    /*!
-      \param stream the current ostream.
-      \param ray the current ray which needs to be printed.
-      \return The information of the object as an ostream.
-    */
+    /**
+     //! An overload of the operator << to print rays for debug.
+     * @brief
+     *
+     * @param stream the current ostream
+     * @param ray the current ray which needs to be printed.
+     * @return std::ostream& the information of the object as an ostream.
+     */
     friend std::ostream &operator<<(std::ostream &stream, Ray const &ray) {
         return stream << "Ray of initPt: " << ray.initPt << " and direction: " << ray.dir;
     }
