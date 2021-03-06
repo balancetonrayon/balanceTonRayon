9c887684fc6704d299500b9b70a24c27f583f1c0
Laurent Olivier <olivier.laurent@ensta-paris.fr>
2021-03-06 20:47:34 +0100

    Parse Textures

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -5,13 +5,14 @@
 Parser::Parser(std::string xmlData) {
     doc.Parse(xmlData.c_str());
     auto scene = doc.FirstChildElement("scene");
-    
+
     // meta
     auto metaTag = scene->FirstChildElement("meta");
     name = metaTag->FirstChildElement("name")->GetText();
     size = getXY(metaTag->FirstChildElement("size"));
+    backgroundColor = getRGB(metaTag->FirstChildElement("background-color"));
     maxDepth = std::stoi(metaTag->FirstChildElement("max_depth")->GetText());
-    
+
     // camera
     auto cameraTag = scene->FirstChildElement("camera");
     auto cameraPos = getXYZ(cameraTag->FirstChildElement("pos"));
@@ -19,8 +20,9 @@ Parser::Parser(std::string xmlData) {
     auto cameraFoc = std::stof(cameraTag->FirstChildElement("foc")->GetText());
     auto cameraSize = getXY(cameraTag->FirstChildElement("size"));
     auto cameraPix = getXY(cameraTag->FirstChildElement("pix"));
-    camera = std::make_shared<Camera>(cameraPos, cameraDir, cameraSize.x, cameraSize.y, cameraPix.x, cameraPix.y, cameraFoc);
-    
+    camera = std::make_shared<Camera>(cameraPos, cameraDir, cameraSize.x, cameraSize.y, cameraPix.x,
+                                      cameraPix.y, cameraFoc);
+
     // lightsources
     auto lightTag = scene->FirstChildElement("lightsources");
     auto dlightTag = lightTag->FirstChildElement("directLight");
@@ -28,27 +30,74 @@ Parser::Parser(std::string xmlData) {
     auto dlightColor = getRGB(dlightTag->FirstChildElement("color"));
     auto dlightIntensity = std::stoi(dlightTag->FirstChildElement("intensity")->GetText());
     sources.push_back(std::make_shared<DirectLight>(dlightPos, dlightColor, dlightIntensity));
-    
+
     // objects
     auto objectsTag = scene->FirstChildElement("objects");
-    for (auto objectTag = objectsTag->FirstChildElement(); objectTag != NULL; objectTag = objectTag->NextSiblingElement()) {
+    for (auto objectTag = objectsTag->FirstChildElement(); objectTag != NULL;
+         objectTag = objectTag->NextSiblingElement()) {
         std::string objectName = objectTag->Name();
-        
+
         // common for all objects
         auto objectPos = getXYZ(objectTag->FirstChildElement("pos"));
         auto objectColor = getRGB(objectTag->FirstChildElement("color"));
-        auto objectTransmission = std::stof(objectTag->FirstChildElement("transmission")->GetText());
+        auto objectTransmission =
+            std::stof(objectTag->FirstChildElement("transmission")->GetText());
         auto objectReflexion = std::stof(objectTag->FirstChildElement("reflexion")->GetText());
         auto objectRefractive = std::stof(objectTag->FirstChildElement("refractive")->GetText());
         auto objectAlbedo = std::stof(objectTag->FirstChildElement("albedo")->GetText());
-        
+
+        bool foundImage = false;
+        bool foundChecked = false;
+        std::shared_ptr<Image> image;
+        std::shared_ptr<CheckedPattern2D> checked;
+    
+        try {
+            auto objectTexture = objectTag->FirstChildElement("texture");
+            if (objectTexture != NULL &&
+                !((std::string)(objectTexture->FirstChildElement()->Name())).compare("image")) {
+                auto imageTag = objectTexture->FirstChildElement("image");
+                auto filename =
+                    "../data/" + (std::string)(imageTag->FirstChildElement("path")->GetText());
+                auto origin = getXYZ(imageTag->FirstChildElement("origin"));
+                auto wVec = getXYZ(imageTag->FirstChildElement("wVec"));
+                auto hVec = getXYZ(imageTag->FirstChildElement("hVec"));
+                image = std::make_shared<Image>(filename, origin, hVec, wVec);
+                foundImage = true;
+            } else if (objectTexture != NULL &&
+                       !((std::string)(objectTexture->FirstChildElement()->Name()))
+                            .compare("checked-pattern-2d")) {
+                auto checkedTag = objectTexture->FirstChildElement("checked-pattern-2d");
+                auto color = getRGB(checkedTag->FirstChildElement("color"));
+                checked = std::make_shared<CheckedPattern2D>(color);
+                foundChecked = true;
+            }
+        } catch (std::exception exc) {
+            -1;  // it doesn't matter
+        }
+
         // object specific
         if (objectName == "plane") {
             auto planeNormal = getXYZ(objectTag->FirstChildElement("normal"));
-            objects.push_back(std::make_shared<Plane>(objectPos, planeNormal, objectColor, objectTransmission, objectRefractive, objectReflexion, objectAlbedo));
+            auto plane =
+                std::make_shared<Plane>(objectPos, planeNormal, objectColor, objectTransmission,
+                                        objectRefractive, objectReflexion, objectAlbedo);
+            if (foundImage) {
+                plane->setTexture(image);
+            } else if (foundChecked) {
+                plane->setTexture(checked);
+            }
+            objects.push_back(plane);
         } else if (objectName == "sphere") {
             auto sphereRadius = std::stof(objectTag->FirstChildElement("radius")->GetText());
-            objects.push_back(std::make_shared<Sphere>(objectPos, objectColor, sphereRadius, objectTransmission, objectRefractive, objectReflexion, objectAlbedo));
+            auto sphere =
+                std::make_shared<Sphere>(objectPos, objectColor, sphereRadius, objectTransmission,
+                                         objectRefractive, objectReflexion, objectAlbedo);
+            if (foundImage) {
+                sphere->setTexture(image);
+            } else if (foundChecked) {
+                sphere->setTexture(checked);
+            }
+            objects.push_back(sphere);
         }
     }
 }
--- src/Parser.hpp
+++ src/Parser.hpp
@@ -1,37 +1,39 @@
 #pragma once
 
-#include "Object.hpp"
-
 #include <string>
-#include <tinyxml2.h>
+
 #include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
+#include <tinyxml2.h>
+
+#include "Object.hpp"
 
 class Parser {
     tinyxml2::XMLDocument doc;
-    
+
     std::string name;
     glm::vec2 size;
     int maxDepth;
-    
+    glm::vec3 backgroundColor;
     std::vector<std::shared_ptr<BasicObject>> objects;
-    
+
     std::vector<std::shared_ptr<Light>> sources;
-    
+
     std::shared_ptr<Camera> camera;
-    
+
 public:
     Parser() = delete;
     Parser(std::string xmlData);
-    
+
     std::string getName() const { return name; }
     glm::vec2 getSize() const { return size; }
     int getMaxDepth() const { return maxDepth; }
-    
-    const std::vector<std::shared_ptr<BasicObject>> & getObjects() const { return objects; }
-    const std::vector<std::shared_ptr<Light>> & getSources() const { return sources; }
-    const std::shared_ptr<Camera> & getCamera() const { return camera; }
-    
+    glm::vec3 getBackgroundColor() const { return backgroundColor; }
+
+    const std::vector<std::shared_ptr<BasicObject>>& getObjects() const { return objects; }
+    const std::vector<std::shared_ptr<Light>>& getSources() const { return sources; }
+    const std::shared_ptr<Camera>& getCamera() const { return camera; }
+
 private:
     glm::vec2 getXY(const tinyxml2::XMLElement* element);
     glm::vec3 getXYZ(const tinyxml2::XMLElement* element);
